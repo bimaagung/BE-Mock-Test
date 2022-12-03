270d6b1649d86f9b65c862d9b1e498ffaed100c2
bimaagung <bimaagung.labs@gmail.com>
2022-12-03 10:05:03 +0700

    delete same parameter in db config production

--- src/config/config.js
+++ src/config/config.js
@@ -14,8 +14,8 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    dialect: process.env.DB_DIALECT || 'postgres',
+    host: process.env.DB_HOSTNAME,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
   },
 };
