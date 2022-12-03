17904c4f5a5ffeed66cd1d620e4544597181309f
bimaagung <bimaagung.labs@gmail.com>
2022-12-03 08:12:34 +0700

    change variable host

--- src/config/config.js
+++ src/config/config.js
@@ -9,4 +9,13 @@ module.exports = {
     port: process.env.DB_PORT || 5432,
     dialect: process.env.DB_DIALECT,
   },
+
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 const port = process.env.PORT || '5000';
 
 app.listen(port, host, () => {
